f49429945c7d774af55a6b74e7990c3d664335ab
Jonathan Rangel <jonathan.rangelrada@gmail.com>
2022-05-15 14:34:34 -0500

    Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'http-post-request';
+@Component({ selector: 'app-root', templateUrl: 'app.component.html' })
+export class AppComponent implements OnInit {
+  objects = [];
+
+
+    constructor(private http: HttpClient) { }
+
+    ngOnInit() {      
+        // Simple POST request with a JSON body and response type <any>
+        this.http.post<any>('https://reqres.in/api/posts', { name: 'cerulean' }).subscribe(data => this.objects = data)
+    }
 }
+
+
